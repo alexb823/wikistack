32ef3a036f628032095c59110513c467b2cfa798
Alex Bukhman <alexb823@gmail.com>
2019-02-10 18:49:46 -0500

    defined data

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const layout = require('./views/layout');
+const { db } = require('./models')
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.urlencoded())
 app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', (req, res)=> {
-  res.send('Hello')
+  res.send(layout('hello world'))
 })
+
+db.authenticate()
+.then(() => console.log('connected to the database'));
 
 const port = process.env.PORT || 3000;
 
