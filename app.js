868e67bc187cc0e9e71c2067763f5c669fa7c25d
Alex Bukhman <alexb823@gmail.com>
2019-02-10 21:53:51 -0500

    added routs for new pages

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const morgan = require('morgan');
 const layout = require('./views/layout');
-const { db } = require('./models')
+const { db } = require('./models');
+const wikiRouter = require('./routes/wiki');
+const userRouter = require('./routes/user')
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
+app.use('/wiki', wikiRouter);
+app.use('/user', userRouter);
 
-app.get('/', (req, res)=> {
-  res.send(layout('hello world'))
-})
-
-db.authenticate()
-.then(() => console.log('connected to the database'));
+app.get('/', (req, res) => {
+  res.send(layout('hello world'));
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const initDb = (force = false) => {
+  db.authenticate()
+    .then(() => console.log('Connected to the database'))
+    .then(() => db.sync({force}))
+    .then(() => app.listen(port, () => console.log(`Listening on port ${port}`)))
+    .catch(error => console.error(error));
+};
+
+initDb(true);
--- models/index.js
+++ models/index.js
@@ -1,16 +1,21 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false,
+});
 
 const Page = db.define('page', {
   title: {
     type: Sequelize.STRING,
+    allowNull: false
   },
   slug: {
     type: Sequelize.STRING,
+    allowNull: false
   },
   content: {
     type: Sequelize.TEXT,
+    allowNull: false
   },
   status: {
     type: Sequelize.ENUM('open', 'closed'),
@@ -20,10 +25,13 @@ const Page = db.define('page', {
 const User = db.define('user', {
   name: {
     type: Sequelize.STRING,
+    allowNull: false
   },
   email: {
     type: Sequelize.STRING,
+    allowNull: false,
+    validate: {isEmail: true}
   },
 });
 
-module.exports = { Page, User };
+module.exports = { db, Page, User };
